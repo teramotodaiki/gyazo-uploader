9039bd4817f123034149cab690fcfe4e75682bfd
teramotodaiki <i101323@gmail.com>
2022-01-30 18:19:45 +0900

    いらんコードを消す

--- gyazo-uploader/ContentView.swift
+++ gyazo-uploader/ContentView.swift
@@ -17,11 +17,6 @@ struct LocalImageAsset {
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     @State var imageAssets: [LocalImageAsset] = []
     
     var body: some View {
@@ -99,7 +94,6 @@ struct ContentView: View {
             }
         })
         task.resume()
-        
     }
     
     // will call after PHPhotoLibrary.requestAuthorization
@@ -120,46 +114,7 @@ struct ContentView: View {
             })
         })
     }
-    
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 private func getAccessToken() -> String {
     var keys: NSDictionary?
